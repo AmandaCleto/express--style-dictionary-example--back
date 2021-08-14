bd276c085cda6e4180922f7658d7995e2e7e4915
AmandaCleto <amanda.clet@gmail.com>
2021-08-14 17:20:44 -0300

    built on route

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const StyleDictionary = require('style-dictionary').extend({
+    source: ['src/tokens/**/*.json'],
+    platforms: {
+      scss: {
+        transformGroup: 'scss',
+        buildPath: 'src/build/scss/',
+        files: [{
+          destination: '_variables.scss',
+          format: 'scss/variables'
+        }]
+      }
+      // ...
+    }
+});
+
+module.exports = StyleDictionary;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var router = express();
+const express = require('express');
+const router = express();
+
+const StyleDictionary = require('../index.js');
+
 
 // respond with "hello world" when a GET request is made to the homepage
 router.get('/', function(req, res) {
+  StyleDictionary.buildAllPlatforms();
   res.send('hello world');
 });
 
