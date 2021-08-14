7f021739c6f3e7298e28eb429763aa6441f24959
AmandaCleto <amanda.clet@gmail.com>
2021-08-14 16:39:12 -0300

    installed express; created first route

---
+++ src/routes/router.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+router.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+module.exports = router;
