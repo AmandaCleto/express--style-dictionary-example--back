3e120b2c3fa811bbb3c8e93f5bb71bdb5404289e
AmandaCleto <amanda.clet@gmail.com>
2021-08-15 18:50:49 -0300

    created route that builds and send file as response

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const app = express();
 const router = express();
+const path = require('path');
 
 const StyleDictionary = require('../index.js');
 
+const folderScss = path.resolve( __dirname, "..", "build", "scss");
+const filePath = `${folderScss}/_variables.scss`;
 
-// respond with "hello world" when a GET request is made to the homepage
-router.get('/', function(req, res) {
-  StyleDictionary.buildAllPlatforms();
-  res.send('hello world');
-});
+router.get('/design-token/scss', function (req, res) {
+    StyleDictionary.buildAllPlatforms();
+    res.sendFile(filePath);
+})
 
 module.exports = router;
